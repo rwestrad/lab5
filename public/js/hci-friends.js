36a2586d209ff2f211b69e80bb8d5854dc1e3879
Ricky Estrada <rickyestrada@Rickys-MacBook-Pro.local>
2019-02-07 12:00:38 -0800

    finished

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,14 +10,21 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+	$("a.click").click(handleClick);
+}
+
+function handleClick(e) {
+		e.preventDefault();
+		var name = $(this).text();
+		$(this).text(anagrammedName(name));
 }
 
 function anagrammedName(name) {
 	// Thanks, Internet Anagram Server!
-	
+
 	if (name == "Doug Engelbart") {
 		return "Notable Grudge";
-	} 
+	}
 	else if (name == "Ivan Sutherland") {
 		return "Vandal Heist Run";
 	}
